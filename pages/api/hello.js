8d054a0644e08d3147a4a1bb0d13b1f579c1bea4
Peter Jones <peter@peter-jones.ca>
2025-08-13 17:11:58 -0400

    remove auth related code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,3 @@
-import { getSession } from "next-auth/react"
-
 export default async function handler(req, res) {
-  const session = await getSession({req});
-
-  if(!session) return res.status(401).send('Unauthorized')
-
   res.status(200).json({ name: 'John Doe' })
 }
